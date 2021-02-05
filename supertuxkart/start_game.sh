368287269af69f2683dbc777be035ff32587a2cd
liuty10 <liuty10@gmail.com>
2021-02-05 12:34:37 -0600

    start stk fullscreen

--- supertuxkart/start_game.sh
+++ supertuxkart/start_game.sh
@@ -7,4 +7,4 @@ GEOMETRY=$(cat ~/.vnc/geometry.cfg)
 #vglrun ./start_game.sh --race-now --mode=0 --track=abyss --demo-laps=99 --demo-karts=5 --kart=sara_the_racer --numkarts=100 --screensize=1280x960
 #vglrun ./start_game.sh --race-now --mode=0 --track=abyss --demo-laps=99 --demo-karts=5 --kart=sara_the_racer --numkarts=100 --screensize=1920x1080
 #vglrun ./start_game_real.sh --race-now --mode=0 --track=abyss --demo-laps=99 --demo-karts=5 --kart=sara_the_racer --numkarts=100 --screensize=1280x720
-vglrun ./start_game_real.sh --race-now --mode=0 --track=abyss --demo-laps=99 --demo-karts=5 --kart=sara_the_racer --numkarts=100 --screensize=$GEOMETRY
+vglrun ./start_game_real.sh --race-now --mode=0 --track=abyss --demo-laps=99 --demo-karts=5 --kart=sara_the_racer --numkarts=100 --screensize=$GEOMETRY --fullscreen
